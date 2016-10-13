a532e4d28a438d4df7ef15b3cfa11210c51ae522
Quentin de Longraye <quentin@dldl.fr>
2016-10-13 21:51:46 +0200

    add anotations, code cleanup ✨

--- src/knapsack/Item.java
+++ src/knapsack/Item.java
@@ -1,29 +1,39 @@
 package knapsack;
 
+/**
+ * Item defined by a weight and a qualitative value.
+ */
 public class Item {
     private int weight;
     private int value;
 
+    /**
+     * Create a new item.
+     *
+     * @param value qualitative value
+     * @param weight item weight
+     */
     public Item(int value, int weight) {
         this.value = value;
         this.weight = weight;
     }
 
-
+    /**
+     * Get the item weight.
+     *
+     * @return the item weight
+     */
     public int getWeight() {
         return weight;
     }
 
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
+    /**
+     * Get the item value.
+     *
+     * @return the item value
+     */
     public int getValue() {
         return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
     }
 
     @Override
--- src/knapsack/Knapsack.java
+++ src/knapsack/Knapsack.java
@@ -4,21 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * Knapsack example using branch & bound algorithm.
+ *
+ * @author Quentin de Longraye <quentin@dldl.fr>
+ * @author Victor Le <m.victor.le@gmail.com>
+ */
 public class Knapsack {
-
     private List<Item> items;
-
     private final List<Item> bag;
-    private final int capacity = 16;
+    private static final int CAPACITY = 16;
 
     private int maxValue = 0;
-
     private int currentWeight = 0;
     private int currentValue = 0;
 
     private List<Item> bestBag;
 
-
+    /**
+     * Create a new Knapsack.
+     *
+     * @param n number of different available items
+     */
     public Knapsack(int n) {
         this.bag = new ArrayList<>();
         this.bestBag = new ArrayList<>();
@@ -26,26 +33,26 @@ public class Knapsack {
         this.initItems(n);
     }
 
+    /**
+     * Init a list of n different items.
+     *
+     * @param n number of different items
+     */
     public void initItems(int n) {
         this.items = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+
+        for (int i = 0; i < n; ++i) {
             int weight = new Random().nextInt(10);
             int value = new Random().nextInt(10);
             this.items.add(new Item(value, weight));
         }
-        /*
-		// ------------- Exemple du professeur -----
-		this.items = new ArrayList<>();
-		this.items.add(new knapsack.Item(45, 3));
-		this.items.add(new knapsack.Item(30, 5));
-		this.items.add(new knapsack.Item(45, 9));
-		this.items.add(new knapsack.Item(10, 5));
-		// ------------- -----------------------------
-*/
-
     }
 
-
+    /**
+     * Add an item to the bag.
+     *
+     * @param item item to be added
+     */
     public void addItemInBag(Item item) {
         this.currentWeight += item.getWeight();
         this.currentValue += item.getValue();
@@ -53,15 +60,18 @@ public class Knapsack {
 
         if (this.currentValue > this.maxValue) {
             this.maxValue = this.currentValue;
-            System.out.print(this.maxValue + " ");
-            System.out.print(this.currentWeight);
-
-
+            System.out.print(this.maxValue + " " + this.currentWeight);
             System.out.println(this.bestBag);
-            this.bestBag = new ArrayList<>(this.bag); // clone
+
+            this.bestBag = new ArrayList<>(this.bag);
         }
     }
 
+    /**
+     * Remove an item form the bag.
+     *
+     * @param item item to be removed
+     */
     public void removeItemInBag(Item item) {
         this.currentWeight -= item.getWeight();
         this.currentValue -= item.getValue();
@@ -69,11 +79,16 @@ public class Knapsack {
     }
 
     public boolean canEnterInBag(Item item) {
-        return item.getWeight() + this.currentWeight <= this.capacity;
+        return item.getWeight() + this.currentWeight <= Knapsack.CAPACITY;
     }
 
+    /**
+     * Find the best item combination using branch & bound.
+     *
+     * @param index current item index
+     */
     public void solve(int index) {
-        if (index == this.items.size()) {
+        if (index >= this.items.size()) {
             return;
         }
 
@@ -81,23 +96,19 @@ public class Knapsack {
 
         if (this.canEnterInBag(item)) {
             this.addItemInBag(item);
-            solve(index + 1);
+            this.solve(index + 1);
             this.removeItemInBag(item);
         }
 
-        solve(index + 1);
+        this.solve(index + 1);
     }
 
     public static void main(String[] args) {
-        Knapsack sack = new Knapsack(100);
-
+        Knapsack knapsack = new Knapsack(100);
         long time = System.nanoTime();
 
-        sack.solve(0);
+        knapsack.solve(0);
 
         System.out.println("Time : " + (System.nanoTime() - time) / 1000000000.0 + " seconds");
-
-
     }
-
 }
